c6ce6c82a8375f161c5ae99f34c756b2c53ef5cd
T-ARCHITECT <t.personal.business@gmail.com>
2025-07-12 19:44:01 +0100

    build: animated gradient bubbles landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,96 @@
-export default function App() {
+"use client";
+
+//? STYLESHEET | Global/Root
+import "@/styles/scss/globals.scss";
+import "@/styles/css/globals.css";
+
+// ? REACT Built-in
+import React, { useEffect } from "react";
+
+export default function App(): React.JSX.Element {
+
+  //? useEffect() Hook - Runs after component mounts.
+  useEffect(() => {
+    //* Selects the DOM element with the class 'interactive'.
+    const interBubble = document.querySelector(".interactive");
+    //* Initializing the current X coordinate to 0.
+    let curX = 0;
+    //* Initializing the current Y coordinate to 0.
+    let curY = 0;
+    //* Initializing the target X coordinate to 0.
+    let tgX = 0;
+    //* Initializing the target Y coordinate to 0.
+    let tgY = 0;
+    
+    //? Function: smoothly move interactive bubble towards the target coordinates.
+    function move() {
+      //* Update current X by moving fraction of distance towards target X.
+      curX += (tgX - curX) / 20;
+      //* Update current Y by moving fraction of distance towards target Y.
+      curY += (tgY - curY) / 20;
+      //* Check if the interactive element is found.
+      if (interBubble) {
+        // @ts-expect-error - CSS Transform
+        //* Apply a CSS transform to move the element.
+        interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+      }
+      //* Request the next animation frame & call move function again.
+      requestAnimationFrame(() => {
+        move();
+      });
+    }
+
+    //* Add an event listener to the window object to listen for mouse movements.
+    window.addEventListener("mousemove", (event) => {
+      //* Update the target X coordinate to the current mouse X position.
+      tgX = event.clientX;
+      //* Update the target Y coordinate to the current mouse Y position. 
+      tgY = event.clientY;
+    });
+
+    //* Start the move function when the component mounts.
+    move();
+  }, []); //? Empty dependency array means this effect runs once, when component mounts.
+
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <main>
+      <div className="landing-text__container">
+        <h1 className="text-white text-2xl sm:text-[2.5rem] md:text-[3rem] lg:text-[4.5rem] xl:text-[4.6rem] mb-4 tracking-in-expand">
+          #PROTOTYPE
+        </h1>
+      </div>
+      {/* Container with a gradient background */}
+      <div className="gradient-background__container">
+        {/* SVG element for filter effects */}
+        <svg xmlns="http://www.w3.org/2000/svg">
+        {/* Definitions for reusable elements within the SVG */}
+          <defs>
+            {/* Filter with id 'goo' */}
+            <filter id="goo">
+              {/* Apply a Gaussian blur to the source graphic */}
+              <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+              <feColorMatrix
+                in="blur"
+                mode="matrix"
+                values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
+                result="goo"
+              /> {/* Apply a color matrix to create a 'gooey' effect */}
+              {/* Blend the original graphic with the goo effect */}
+              <feBlend in="SourceGraphic" in2="goo" />
+            </filter>
+          </defs>
+        </svg>
+        {/* Placeholder divs, for graphicS elements */}
+        <div>
+          <div className="g1"></div>
+          <div className="g2"></div>
+          <div className="g3"></div>
+          <div className="g4"></div>
+          <div className="g5"></div>
+          {/* Interactive div that will follow the mouse movements */}
+          <div className="interactive"></div>
+        </div>
+      </div>
+    </main>
   );
 }
